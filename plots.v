a272f16b90ee1ded90870c8762752a0d6c084d4f
Andreas Evensen <andreas.evensen11@gmail.com>
2024-03-07 16:54:21 +0100

    cleanup

--- plots.v
+++ plots.v
@@ -1,1 +1,50 @@
 module vtikz
+
+struct Function_plot {
+	func string
+	color string
+}
+
+struct Scatter_plot {
+	x []f32
+	y []f32
+	color string
+mut:
+	mark Mark_style = .line
+}
+
+struct Function3d {
+	func string
+	type_ Plot3d_type
+}
+
+struct Scatter3d {
+	z []f32
+	x []f32
+	y []f32
+	type_ Plot3d_type
+}
+
+pub enum Plot3d_type {
+	mesh	
+	surface
+	line
+	scatter
+}
+
+fn (p Plot3d_type) to_string() string {
+	return match p {
+		.mesh { 'mesh' }
+		.surface { 'surf' }
+		.line { 'line' }
+		.scatter { 'scatter' }
+	}
+}
+type Plot_type = Scatter_plot | Function_plot | Scatter3d | Function3d
+
+struct Plot {
+	legend string
+mut:
+	plot Plot_type
+	style string = 'solid'
+}
