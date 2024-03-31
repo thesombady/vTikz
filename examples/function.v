5a85a6e719861999ec624f664ca2719a53610ebd
Andreas Evensen <andreas.evensen11@gmail.com>
2024-03-31 20:53:00 +0200

    Examples: remade one example

--- examples/function.v
+++ examples/function.v
@@ -3,13 +3,12 @@ import vtikz
 // Define the tikz object
 
 mut tikz := vtikz.Tikz.new(
-	'xlabel',
-	'ylabel',
-	'title'
+	'x [m/s]',
+	'y [m]',
+	'Velocity Squared',
 )
 
 /*
-
 	tikz.add_function(
 		function: string, e.g. 'x^2'
 		color: string, e.g. 'red'
@@ -19,7 +18,7 @@ mut tikz := vtikz.Tikz.new(
 */
 tikz.add_function('x^2', 'red', '$ x^2$', [f32(-3), 3]!)
 // Even though we give it a legend, it will not be shown unless we force it, or give another function
-// tikz.pref.show_legend = true
+// tiks.set_show_legends(true)
 
 tikz.plot('squared.tex')
 // This will create a pdf file called squared.pdf
